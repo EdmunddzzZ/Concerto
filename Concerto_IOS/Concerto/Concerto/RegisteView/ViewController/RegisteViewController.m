f9ef6a8d4d8c5d596fb11fd6a3d6fdf9df981af1
EdmunddzzZ <>
2021-04-23 17:15:51 +0800

    注册页

--- Concerto_IOS/Concerto/Concerto/RegisteView/ViewController/RegisteViewController.m
+++ Concerto_IOS/Concerto/Concerto/RegisteView/ViewController/RegisteViewController.m
@@ -7,6 +7,7 @@
 
 #import "RegisteViewController.h"
 #import "CreateBase.h"
+#import "ZXCountDownBtn.h"
 @interface RegisteViewController ()
 @property(nonatomic,strong)UIView *topBar;
 @property(nonatomic,strong)UIImageView * icon;
@@ -15,7 +16,13 @@
 @property(nonatomic,strong)UITextField * code;
 @property(nonatomic,strong)UITextField * psw;
 @property(nonatomic,strong)UITextField * repsw;
-@property(nonatomic,strong)UIButton *sendMsg;
+@property(nonatomic,strong)ZXCountDownBtn *sendMsg;
+@property(nonatomic,strong)UIView *agreeView;
+@property(nonatomic,assign)bool isAgree;
+@property(nonatomic,strong)UIButton *agree;
+@property(nonatomic,strong)UIButton *registe;
+@property(nonatomic,strong)UIButton *backToLogin;
+
 @end
 
 @implementation RegisteViewController
@@ -25,15 +32,129 @@
     self.view.backgroundColor = [CreateBase createColor:252 blue:247 green:234];
     [self.view addSubview:self.topBar];
     [self.view addSubview:self.icon];
-    [self.view addSubview:_inputView];
+    [self.view addSubview:self.inputView];
+    [self.view addSubview:self.agreeView];
+    [self.view addSubview:self.registe];
+    [self.view addSubview:self.backToLogin];
+    [self.sendMsg setCountDown:60 mark:@"GetCodeBtn" resTextFormat:^NSString * _Nullable(long remainSec) {
+        if(remainSec)
+        {
+            [self.sendMsg setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+            [self.sendMsg.layer setBorderColor:[UIColor grayColor].CGColor];
+            [self.sendMsg setEnabled:NO];
+        }
+        else
+        {
+            [self.sendMsg setTitleColor:[CreateBase createColor:101 blue:206 green:185] forState:UIControlStateNormal];
+            [self.sendMsg.layer setBorderColor:[CreateBase createColor:101 blue:206 green:185].CGColor];
+            [self.sendMsg setEnabled:YES];
+        }
+        return [NSString stringWithFormat:@"%ld秒后重发",remainSec];
+    }];
     // Do any additional setup after loading the view.
+}
+-(UIButton *)backToLogin
+{
+    if(!_backToLogin)
+    {
+        _backToLogin = [[UIButton alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.registe.frame)+20, screenwith, 15)];
+        [_backToLogin setTitleColor:[CreateBase createColor:101 blue:206 green:185] forState:UIControlStateNormal];
+        [_backToLogin.titleLabel setFont:[UIFont systemFontOfSize:14]];
+        [_backToLogin addTarget:self action:@selector(backToPreView) forControlEvents:UIControlEventTouchUpInside];
+        [_backToLogin setTitle:@"已有账户？立即登录" forState:UIControlStateNormal];
+    }
+    return _backToLogin;
+}
+-(UIButton *)registe
+{
+    if(!_registe)
+    {
+        _registe = [[UIButton alloc]initWithFrame:CGRectMake(self.inputView.frame.origin.x, CGRectGetMaxY(self.agreeView.frame)+25, self.inputView.frame.size.width, 55)];
+        [_registe setBackgroundColor:[CreateBase createColor:101 blue:206 green:185]];
+        [_registe setTitle:@"注册" forState:UIControlStateNormal];
+        [_registe.layer setCornerRadius:5];
+        [_registe.layer setMasksToBounds:YES];
+    }
+    return _registe;
+}
+-(UIView *)agreeView
+{
+    if(!_agreeView)
+    {
+        _agreeView = [[UIView alloc]initWithFrame:CGRectMake(self.inputView.frame.origin.x+20, CGRectGetMaxY(self.inputView.frame)+30, self.inputView.frame.size.width-40, 30)];
+//        _agreeView.backgroundColor = [UIColor whiteColor];
+        _agree = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 18, 18)];
+        //[_agree setBackgroundColor:[CreateBase createColor:101 blue:206 green:185]];
+        [_agree.layer setCornerRadius:9];
+        [_agree addTarget:self action:@selector(agreeClick) forControlEvents:UIControlEventTouchUpInside];
+        _isAgree = NO;
+        [_agree.layer setBorderColor:[CreateBase createColor:101 blue:206 green:185].CGColor];
+        [_agree.layer setBorderWidth:1];
+        [_agree.layer setMasksToBounds:YES];
+        [_agreeView addSubview:_agree];
+        UILabel *settlement = [[UILabel alloc]initWithFrame:CGRectMake(CGRectGetMaxX(_agree.frame)+10, _agree.frame.origin.y, 200, 18)];
+        NSMutableAttributedString *str = [[NSMutableAttributedString alloc]initWithString:@"同意《用户服务协议》"];
+        [str addAttribute:NSForegroundColorAttributeName value:[CreateBase createColor:101 blue:206 green:185] range:NSMakeRange(3, 6)];
+        settlement.attributedText = str;
+        settlement.userInteractionEnabled = YES;
+        UITapGestureRecognizer* tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(takeToSettlement)];
+        [settlement addGestureRecognizer:tap];
+        [_agreeView addSubview: settlement];
+    }
+    return _agreeView;
+}
+-(void)takeToSettlement
+{
+    NSLog(@"带我去服务协议");
 }
 -(UIView *)inputView
 {
     if(!_inputView)
     {
-        _inputView = [[UIView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(self.icon.frame)+50, screenwith-80, 320)];
+        _inputView = [[UIView alloc]initWithFrame:CGRectMake(40, CGRectGetMaxY(self.icon.frame)+50, screenwith-80, 65*4)];
         _inputView.backgroundColor = [UIColor whiteColor];
+        _inputView.layer.cornerRadius = 5;
+        _inputView.layer.masksToBounds = YES;
+        _email = [[UITextField alloc]initWithFrame:CGRectMake(_inputView.frame.size.width*0.2, 15, _inputView.frame.size.width*0.8, 35)];
+        _email.placeholder = @"请输入邮箱地址";
+        _email.font = [UIFont systemFontOfSize:14];
+        [_inputView addSubview:_email];
+        _inputView.layer.borderWidth = 1;
+        _inputView.layer.borderColor = [CreateBase createColor:228].CGColor
+        ;
+        UILabel *line1 = [[UILabel alloc]initWithFrame:CGRectMake(0, 65, screenwith, 1)];
+        line1.backgroundColor = [CreateBase createColor:228];
+        
+        UILabel *line2 = [[UILabel alloc]initWithFrame:CGRectMake(0, 130, screenwith, 1)];
+        line2.backgroundColor = [CreateBase createColor:228];
+        UILabel *line3 = [[UILabel alloc]initWithFrame:CGRectMake(0, 195, screenwith, 1)];
+        line3.backgroundColor = [CreateBase createColor:228];
+        [_inputView addSubview:line1];
+        [_inputView addSubview:line2];
+        [_inputView addSubview:line3];
+        _code = [[UITextField alloc]initWithFrame:CGRectMake(_inputView.frame.size.width*0.2, CGRectGetMaxY(line1.frame)+15, _inputView.frame.size.width*0.4, 35)];
+        _code.placeholder = @"请输入邮箱验证码";
+        _code.font = [UIFont systemFontOfSize:14];
+        _psw = [[UITextField alloc]initWithFrame:CGRectMake(_inputView.frame.size.width*0.2, CGRectGetMaxY(line2.frame)+15, _inputView.frame.size.width*0.8, 35)];
+        _psw.font = [UIFont systemFontOfSize:14];
+        _psw.placeholder = @"请设置6-20位登录密码";
+        _psw.secureTextEntry = YES;
+        _repsw = [[UITextField alloc]initWithFrame:CGRectMake(_inputView.frame.size.width*0.2, CGRectGetMaxY(line3.frame)+15, _inputView.frame.size.width*0.8, 35)];
+        _repsw.secureTextEntry = YES;
+        _repsw.placeholder = @"请再次确认密码";
+        _repsw.font = [UIFont systemFontOfSize:14];
+        [_inputView addSubview:_repsw];
+        [_inputView addSubview:_psw];
+        [_inputView addSubview:_code];
+        _sendMsg = [[ZXCountDownBtn alloc]initWithFrame:CGRectMake(CGRectGetMaxX(_code.frame)+30, _code.frame.origin.y, _inputView.frame.size.width*0.25, 35)];
+        [_sendMsg setTitle:@"发送验证码" forState:UIControlStateNormal];
+        [_sendMsg.layer setBorderWidth:1];
+        [_sendMsg.layer setBorderColor:[CreateBase createColor:101 blue:206 green:185].CGColor];
+        [_sendMsg.titleLabel setFont:[UIFont systemFontOfSize:14]];
+        [_sendMsg.layer setCornerRadius:5];
+        [_sendMsg setTitleColor:[CreateBase createColor:101 blue:206 green:185] forState:UIControlStateNormal];
+        [_sendMsg addTarget:self action:@selector(sendmsg) forControlEvents:UIControlEventTouchUpInside];
+        [_inputView addSubview:_sendMsg];
         
     }
     return _inputView;
@@ -76,6 +197,24 @@
 -(void)backToPreView
 {
     [CreateBase backToPreView];
+}
+-(void)agreeClick
+{
+    if(self.isAgree == NO)
+    {
+        [self.agree setBackgroundColor:[CreateBase createColor:101 blue:206 green:185]];
+        self.isAgree = YES;
+    }
+    else
+    {
+        [self.agree setBackgroundColor:[UIColor clearColor]];
+        self.isAgree = NO;
+    }
+}
+-(void)sendmsg
+{
+    [self.view endEditing:YES];
+    [self.sendMsg startCountDown];
 }
 /*
 #pragma mark - Navigation
